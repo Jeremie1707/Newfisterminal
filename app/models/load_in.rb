ee7d445a60af5a157045e7e8791b51c8ca1a0e4f
Jeremie Poiraudau <jeremie.poiraudau@lafokale.com>
2019-12-18 10:12:55 +0100

    search and pagination

--- app/models/load_in.rb
+++ app/models/load_in.rb
@@ -3,7 +3,7 @@ class LoadIn < ApplicationRecord
   belongs_to :t1_customer
   has_many :in_assignments
 
-  pg_search_scope :global_search, against: [:reference], associated_against: {
+  pg_search_scope :global_search, against: [:reference, :status, :truck_nr, :trailer_nr, :arrival_date ], associated_against: {
       in_assignments: [:reference, :lot_nr, :incoming_order_ref, :other_ref, :number_of_boxe, :number_of_pallet],
       t1_customer: [:name]
      },
