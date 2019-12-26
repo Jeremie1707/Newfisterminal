7aac460a38debffc7696105291be204571f9b135
Jeremie Poiraudau <jeremie.poiraudau@lafokale.com>
2019-12-26 13:26:42 +0100

    form working

--- app/controllers/load_ins_controller.rb
+++ app/controllers/load_ins_controller.rb
@@ -5,6 +5,8 @@ class LoadInsController < ApplicationController
   end
 
   def create
+    p "hello strong params"
+    p strong_params
     @load_in = LoadIn.new(strong_params)
     p @load_in
     if @load_in.save!
@@ -18,9 +20,7 @@ class LoadInsController < ApplicationController
 
   def strong_params
     params.require(:load_in).permit(
-    :t1_customer_id, :status, :arrival_date, :truck_nr, :trailer_nr, :type_of_service,
-    in_assignments_attributes: [:id, :packer, :lot_nr, :incoming_order_ref, :other_ref, :number_of_boxe, :number_of_pallet]
+    :t1_customer_id, :status, :arrival_date, :truck_nr, :trailer_nr, :type_of_service, :in_assignments_attributes => [ :packer_id, :lot_nr, :incoming_order_ref, :other_ref]
   )
   end
-
 end
